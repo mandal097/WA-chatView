bed6c79dd804e1a901df601d9dccbc4c4a7fb5a8
amarnath <amarnathmandal097@gmail.com>
2023-01-17 19:57:04 +0530

    creating function in resetpassword.jsx for changing password from backend

--- src/pages/Auth/ResetPassword/ResetPassword.jsx
+++ src/pages/Auth/ResetPassword/ResetPassword.jsx
@@ -2,22 +2,71 @@ import React, { useState } from 'react';
 import styles from './ResetPassword.module.scss';
 import AuthLayout from '../AuthLayout'
 import { InputPassword, Submit } from '../../../components/Auth/Inputs';
+import { toast, ToastContainer } from 'react-toastify';
+import axios from '../../../config/axios';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../../redux/userRedux';
+import { removeMembers } from '../../../redux/AddToGroup';
+import { setCurrentChatInitial } from '../../../redux/chatRedux';
+import { useNavigate } from 'react-router-dom';
 
 const ResetPassword = () => {
     const [oldPassword, setOldPassword] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [loading, setLoading] = useState(false);
+    const dispatch = useDispatch();
+    const navigate = useNavigate()
 
-    const submit = (e) => {
+    const logoutUser = () => {
+        localStorage.removeItem('token');
+        localStorage.clear();
+        dispatch(logout());
+        dispatch(removeMembers());
+        dispatch(setCurrentChatInitial())
+        navigate('/login');
+    };
+
+
+    const submit = async (e) => {
         e.preventDefault();
-        setLoading(true);
-        setTimeout(() => {
-            setLoading(false)
-        }, 2000);
+        if (!oldPassword || !password || !confirmPassword) {
+            return toast.error('All fields are required');
+        }
+        if (password !== confirmPassword) {
+            return toast.error('Password mismatched')
+        }
+        try {
+            setLoading(true);
+
+            const res = await axios.put('/user/reset-password', { password, oldPassword }, {
+                headers: {
+                    token: `Bearer ${localStorage.getItem('token')}`
+                }
+            });
+
+            if (res.data.status === 'err') {  //if backend error comes 
+                toast.error(res.data.message);
+                setLoading(false);
+            }
+            if (res.data.status === 'success') { //if  password successfully updated
+                toast.success(res.data.message);
+                setLoading(false);
+                setTimeout(() => {
+                    setLoading(false);
+                    logoutUser();
+                }, 2000);
+            };
+
+        } catch (error) {
+            toast.error('Something went wrong')
+            setLoading(false);
+        }
+
     }
     return (
         <div className={styles.reset_password}>
+            <ToastContainer className='toaster' />
             <AuthLayout heading='WeChat'>
                 <h4>Reset Password</h4>
                 <form onSubmit={submit}>
