a16cc10629e57ebc30a8944bd6f37a6166ad21da
amarnath <amarnathmandal097@gmail.com>
2023-01-23 11:39:23 +0530

    searchbox added in shop navbar component

--- src/components/Shop/ShopNavbar/ShopNavbar.jsx
+++ src/components/Shop/ShopNavbar/ShopNavbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from './ShopNavbar.module.scss'
 import {
+    LoginOutlined,
     // ArrowLeftOutlined,
     LogoutOutlined,
     QqOutlined,
+    SearchOutlined,
     ShoppingCartOutlined
 } from '@ant-design/icons';
 import { useDispatch, useSelector } from 'react-redux';
@@ -18,7 +20,7 @@ const ShopNavbar = () => {
 
     const dispatch = useDispatch();
 
-    
+
     const logoutUser = () => {
         localStorage.removeItem('token');
         localStorage.clear();
@@ -35,23 +37,30 @@ const ShopNavbar = () => {
                 <div className={styles.logo} onClick={() => navigate('/shop')}>
                     <QqOutlined className='icon' />
                 </div>
-                <div className={styles.middle}></div>
+                <div className={styles.middle}>
+                    <SearchOutlined className={styles.icon}/>
+                    <input type="text" placeholder='what you are looking for...' />
+                </div>
                 <div className={styles.right}>
-                    {/* <button className={styles.auth_btn} onClick={() => {
-                        navigate('/')
-                        window.location.reload()
-                    }}>
-                        <ArrowLeftOutlined className={styles.icon} /><span>Back</span>
-                    </button> */}
-                    <button className={styles.auth_btn} onClick={logoutUser}>
-                        <LogoutOutlined className={styles.icon} /><span>Logout</span>
-                    </button>
+
+                    {
+                        user
+                            ? <button className={styles.auth_btn} onClick={logoutUser}>
+                                <LogoutOutlined className={styles.icon} /><span>Logout</span>
+                            </button>
+                            : <button className={styles.auth_btn}
+                                onClick={() => navigate('/login')}>
+                                <LoginOutlined className={styles.icon} /><span>Login</span>
+                            </button>
+
+                    }
+
                     <button onClick={() => navigate('/shop/cart')}><ShoppingCartOutlined className={styles.icon} /></button>
                     <button onClick={() => {
                         navigate(`/profile/${user?._id}`)
                         window.location.reload()
                     }}>
-                        <img src={user?.profilePic} alt="" />
+                        <img src={user?.profilePic ? user.profilePic : 'https://img.freepik.com/free-psd/3d-illustration-person_23-2149436192.jpg?size=338&ext=jpg&ga=GA1.1.148670595.1674319787'} alt="" />
                     </button>
                 </div>
             </div>
