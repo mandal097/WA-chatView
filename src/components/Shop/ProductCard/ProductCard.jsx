5bf12b4e32b5130bb63f0f74618b2d7dd161d3a9
amarnath <amarnathmandal097@gmail.com>
2023-02-07 17:32:50 +0530

    adding ->wishlist / add-to-cart/ buy-now<- buttons in product card for shop

--- src/components/Shop/ProductCard/ProductCard.jsx
+++ src/components/Shop/ProductCard/ProductCard.jsx
@@ -1,38 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './ProductCard.module.scss';
-import { QqOutlined, StarFilled } from '@ant-design/icons'
+import { HeartFilled, QqOutlined, StarFilled } from '@ant-design/icons'
 import { capitalizeFirstLetter } from '../../../helpers/strings';
+import { Link } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
 
 const ProductCard = () => {
+    const [added, setAdded] = useState(false);
+    const handleWishlist = (e) => {
+        e.stopPropagation()
+        switch (added) {
+            case false:
+                setAdded(true);
+                toast.success('Added to wishlist')
+                break;
+            case true:
+                setAdded(false);
+                toast.error('Removed from wishlist')
+                break;
+            default:
+                setAdded(false)
+        }
+    }
     return (
-        <div className={styles.product_card}>
-            <div className={styles.wrapper}>
-                <div className={styles.img}>
-                    <img src="https://rukminim1.flixcart.com/image/612/612/kv9urgw0/shoe/t/h/b/7-new-9309-7-world-wear-footwear-black-original-imag87ndjqrzjg2m.jpeg?q=70" alt="product_image" />
+        <>
+            <ToastContainer className='toaster' />
+            <div className={styles.product_card}>
+                <div className={styles.wrapper}>
+                    <div className={styles.img}>
+                        <div className={`${styles.wishlist} ${added && 'pop'}`} onClick={handleWishlist} >
+                            <HeartFilled className={`${styles.icon} ${added && styles.added} `} />
+                        </div>
+                        <img src="https://rukminim1.flixcart.com/image/612/612/kv9urgw0/shoe/t/h/b/7-new-9309-7-world-wear-footwear-black-original-imag87ndjqrzjg2m.jpeg?q=70" alt="product_image" />
+                    </div>
+
+                    <div className={styles.details}>
+                        <Link className={styles.name}>Name of the product should be here</Link>
+                        <div className={styles.ratings}>
+                            <div className={styles.stars}>
+                                <span>4.3</span>
+                                <StarFilled className={styles.icon} />
+                            </div>
+                            <small className={styles.counts}>{'(23,343)'}</small>
+                            <div className={styles.logo}>
+                                <QqOutlined className={styles.icon} />
+                            </div>
+                        </div>
+                        <div className={styles.prices}>
+                            <span>$267</span>
+                            <strike>$400</strike>
+                            <small>58% off</small>
+                        </div>
+                        <div className={styles.btns}>
+                            <button>Add to cart</button>
+                            <button>Buy now</button>
+                        </div>
+                        <div className={styles.delivery_stat}><span>{capitalizeFirstLetter("free delivery")}</span></div>
+                    </div>
                 </div>
 
-                <div className={styles.details}>
-                    <span className={styles.name}>Name of the product should be here</span>
-                    <div className={styles.ratings}>
-                        <div className={styles.stars}>
-                            <span>4.3</span>
-                            <StarFilled className={styles.icon} />
-                        </div>
-                        <small className={styles.counts}>{'(23,343)'}</small>
-                        <div className={styles.logo}>
-                            <QqOutlined className={styles.icon} />
-                        </div>
-                    </div>
-                    <div className={styles.prices}>
-                        <span>$267</span>
-                        <strike>$400</strike>
-                        <small>58% off</small>
-                    </div>
-                    <div className={styles.delivery_stat}><span>{capitalizeFirstLetter("free delivery")}</span></div>
-                </div>
             </div>
-
-        </div>
+        </>
     )
 }
 
