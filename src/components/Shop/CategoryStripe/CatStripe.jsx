6a32b6d021ed37da1b845399f57c5568bf04b577
amarnath <amarnathmandal097@gmail.com>
2023-02-07 11:36:02 +0530

    adding animation on catStripe.jsx component on categories page

--- src/components/Shop/CategoryStripe/CatStripe.jsx
+++ src/components/Shop/CategoryStripe/CatStripe.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { createSearchParams, useNavigate } from 'react-router-dom';
 import styles from './CatStripe.module.scss';
 
@@ -28,8 +28,33 @@ const Category = ({ ele, type }) => {
 
 
 const CatStripe = ({ type }) => {
+    // const [scroll, setScroll] = useState(false)
+    const ref = useRef(null);
+    console.log(ref.current)
+    useEffect(() => {
+        var prevScrollPosition = window.pageYOffset;
+        const handler = () => {
+            var currentScrollPosition = window.pageYOffset;
+            if (type === 'categories_page') {
+                if (prevScrollPosition > currentScrollPosition) {
+                    ref.current.style.position = 'fixed'
+                    ref.current.style.top = '60px'
+                    // ref.current.style.transform = 'translateY(12rem)'
+                } else {
+                    ref.current.style.position = 'fixed'
+                    ref.current.style.top = '-60px'
+                    // ref.current.style.transform = 'translateY(-12rem)'
+                }
+            }
+            prevScrollPosition = currentScrollPosition;
+        }
+        window.addEventListener('scroll', handler)
+        return () => window.removeEventListener('scroll', handler)
+    }, [type])
+
     return (
         <div
+            ref={ref}
             className={styles.categories}
             style={{
                 height: type === 'categories_page' && 'auto',
